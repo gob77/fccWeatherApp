2df058caeb3807c98e92b4c71e749020535d6a72
gob77 <mraortiz@hotmail.com>
2017-09-28 13:30:54 -0300

    added some basica style

---
+++ script.js
@@ -0,0 +1,64 @@
+  var currentWeather;
+  var forecastWeather;
+  var c = function(pos){
+      lat = parseFloat(pos.coords.latitude).toFixed(2);
+      lon = parseFloat(pos.coords.longitude).toFixed(2);
+      getWeather(lat, lon, renderWeather);
+    }
+
+
+
+function getLocation(){
+  navigator.geolocation.getCurrentPosition(c);
+  return false;
+}
+
+function getWeather(lat, lon, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        currentWeather = JSON.parse(xhr.responseText);
+        getFormattedDate(currentWeather.dt*1000);
+        document.getElementById("location").innerHTML = currentWeather.name + " , " + currentWeather.sys.country;
+        document.getElementById("weather").innerHTML = getFormattedDate(currentWeather.dt) + "Temp: " + currentWeather.main.temp + " Cº";
+        document.getElementById("weather").classList.add("celsius");
+        document.getElementById("weather-description").innerHTML = currentWeather.weather[0].description;
+        callback(currentWeather);
+    }
+  }
+  xhr.open('GET', "https://fcc-weather-api.glitch.me//api/current?lon=" + lon + "&lat=" + lat, true);
+  xhr.send(null);
+}
+
+function renderWeather(currentWeather) {
+  var iconTpl = getWeatherTemplate(currentWeather.weather[0].main);
+  document.getElementById("weather-icon").innerHTML = iconTpl;
+}
+
+function celsiusToFarenheit() {
+  // debugger;
+  var weatherElement = document.getElementById("weather");
+  if(weatherElement.classList.contains("celsius")) {
+    weatherElement.innerHTML = getFormattedDate(currentWeather.dt) +  parseFloat(currentWeather.main.temp * 1.8 + 32).toFixed(2) + " Fº";
+  }
+  else {
+    weatherElement.innerHTML = getFormattedDate(currentWeather.dt) + currentWeather.main.temp + " Cº";
+  }
+  weatherElement.classList.toggle("celsius")
+  weatherElement.classList.toggle("farenheit")
+}
+
+function getFormattedDate(date){
+  var fullDate = new Date(date);
+      date = fullDate.getDate(),
+      month = fullDate.getMonth() + 1,
+      year = fullDate.getFullYear();
+      days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+      dayOfWeek = days[fullDate.getDay()]
+      return dayOfWeek + " - " + date + " / " + "0" + month + " / " + year + " ";
+}
+
+var CtoF = document.getElementById("CtoF");
+CtoF.addEventListener("click", celsiusToFarenheit);
+
+document.addEventListener("DOMContentLoaded", getLocation);
---
+++ weather.js
@@ -0,0 +1,43 @@
+var weathers = {
+  "Rain": '<div class="icon rainy"> '+
+              ' <div class="cloud"></div>'+
+              '  <div class="rain"></div>'+
+              '</div>'
+  ,
+  "Snow":  '<div class="icon flurries"> '+
+              ' <div class="cloud"></div>'+
+              '  <div class="snow">'+
+              '    <div class="flake"></div>'+
+              '    <div class="flake"></div>'+
+              '  </div>'+
+              '</div>'
+  ,
+  "Clear": '<div class="icon"> '+
+              '<div class="sun"> '+
+                '<div class="rays"></div> '+
+              '</div> '+
+            '</div>'
+  ,
+  "Thunderstorm": '<div class="icon"> '+
+                    '<div class="cloud"></div> '+
+                    '<div class="lightning"> '+
+                      '<div class="bolt"></div> '+
+                      '<div class="bolt"></div> '+
+                    '</div> '+
+                  '</div>'
+  ,
+  "Clouds": '<div class="icon"> '+
+              '<div class="cloud"></div> '+
+              '<div class="cloud"></div> '+
+            '</div>'
+  };
+
+function getWeatherTemplate(weatherKey){
+  return weathers[weatherKey] || "No weather found";
+}
+
+
+// <div class="icon rainy">
+//   <div class="cloud"></div>
+//   <div class="rain"></div>
+// </div>
